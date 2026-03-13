14dc5dbda50eed5fedf831c6305602d49a308f94
c22ug152cap073 <summiayasummiaya2004@gmail.com>
2026-03-13 10:00:01 +0530

    added the file

--- chatverse/chatverse/urls.py
+++ chatverse/chatverse/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from chatbot.views import chat
+from chatbot.views import chat, dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', chat, name='chat'),
+    path('dashboard/', dashboard, name='dashboard'),
 ]
